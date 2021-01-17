5a798a17f458ebb039f5bd3f36fe2d524c627991
FernandoJRR <37566757+FernandoJRR@users.noreply.github.com>
2021-01-16 23:05:05 -0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -29,7 +29,7 @@ function setPosition(position) {
 
 // Get the Weather data
 function getWeather(latitude, longitude) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
   console.log(api);
 
